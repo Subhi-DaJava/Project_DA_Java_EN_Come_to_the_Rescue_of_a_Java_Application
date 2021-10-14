88448c9cbf0d0242e9cba4dc8fad20b36a470823
Subhi-DaJava <subhyfr809@gmail.com>
2021-10-14 23:23:56 +0200

    Apres évaluation, créé une classe, des méthodes pour réutilisées

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,9 +2,12 @@ package com.hemebiotech.analytics;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 /**
  * Lire un fichier dans un dossier, puis le transmet comme les éléments d'une List, enlever les doublons, ranger les éléments 
@@ -14,35 +17,61 @@ import java.util.Set;
  *
  */
 public class AnalyticsCounter {	
-	public static void main(String args[]) throws IOException {
-		
-		//Créer une nouvelle liste qui reçoit tous les sysmptôms
-		List<String> list = new ArrayList<>();
-		
-		//Affecter le chemin du fichier à une chaine de carachtères, puis créer un objet qui transforme chaque symptôme en les éléments de cette list
-		String fileInputPath = "Project02Eclipse/symptoms.txt";
-		ReadSymptomDataFromFile fileToArray = new ReadSymptomDataFromFile(fileInputPath);
-
-		//Utiliser la méthode getSymptoms() de la classe ReadSymptomDataFile qui affecte cette list;		
-		list=fileToArray.getSymptoms();
-		
-		//Générer un fichier de sortie : emplacement du fichier de sortie et instance de la classe FileWriter
-		String fileOutPath="result.out";
-		FileWriter writer = new FileWriter(fileOutPath);
+	String inputFileName, outputFileName;
+	List<String> list = new ArrayList<>();
 	
-		//Ranger les éléments de cette list par l'ordre alphabétique, puis Donner ses éléments à un LinkedHashList(une liste ordonnée) 
-		//c'est pour éliminer les doublons de la list;
-		Collections.sort(list);
-		Set<String> set = new LinkedHashSet<>();
-		set.addAll(list);
+	public AnalyticsCounter(String inputFileName, String outputFileName) {
+		this.inputFileName=inputFileName;
+		this.outputFileName=outputFileName;
 		
-		//Faire écrire chaque élément du set par ligne avec son nombre de l'occurrence dans le fichier de sortie et afficher le même résultat dans la console
-		for(String strLine : set) {
-			writer.write(strLine+"="+Collections.frequency(list,strLine)+";\n");
-			System.out.println(strLine+"="+Collections.frequency(list,strLine)+";");
-		}
-		
-		writer.close();
-			
 	}
+	
+	/**
+	 * 
+	 * @param inputFileName
+	 */
+	public List<String> inputFile(String inputFileName) {
+	ISymptomReader iSymptomReader = new ReadSymptomDataFromFile(inputFileName);
+	this.list = iSymptomReader.getSymptoms();
+	this.inputFileName = inputFileName;
+	return list;
+	}
+	//Affecter le chemin du fichier à une chaine de carachtères, puis créer un objet qui transforme chaque symptôme en les éléments de cette list
+	
+	public void writeFilePath(String outputFileName) {
+	outputFileName="result.out";
+	FileWriter writer = new FileWriter(outputFileName);
+	this.outputFileName =outputFileName;
+	writer.close();
+	}
+	
+	public List<String> sort(List<String> list) {
+		this.list=list;
+		Collections.sort(list);
+		return list;
+		
+		}
+public static Map<String, Integer> listToMap(List<String> list) {
+		
+		Map<String, Integer> map = new HashMap<>();
+	
+		for(int i=0; i < list.size(); i++) {
+			String key= list.get(i);
+			if(!map.containsKey(key)) {
+				map.put(key,1);
+			}
+			else {
+				map.put(list.get(i),map.get(key)+1);
+			}
+			
+		}
+		return map;
+	}
+	//Créer une nouvelle liste qui reçoit tous les sysmptôms
+	//List<String> list = new ArrayList<>();
+		
+	for(String strLine : set) {
+		
+		}
+					
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Application.java
@@ -0,0 +1,24 @@
+package com.hemebiotech.analytics;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * getSymptoms(); countSymptoms();sortSymptoms();writeSymptoms();
+ * String fileName; List<String>; String inputFileName; String outputFileName; 
+ * @author zulfy
+ *
+ */
+public class Application {
+	
+	public static void main(String[] args) throws IOException{
+		
+		//AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+		
+		//outputFileName= "Project02Eclipse/symptoms.txt";
+
+}
