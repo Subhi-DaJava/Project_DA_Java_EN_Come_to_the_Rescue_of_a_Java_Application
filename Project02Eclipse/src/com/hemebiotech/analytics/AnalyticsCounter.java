f5afe1d75db23fa1e42409d86da59d1cc7bd41d5
Subhi-DaJava <subhyfr809@gmail.com>
2021-10-17 14:47:09 +0200

    rajout de result.out dans .gitignore

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,4 +1,5 @@
 package com.hemebiotech.analytics;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -6,17 +7,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
- 
+public class AnalyticsCounter {
 
-public class AnalyticsCounter {	
-	
 	private String inputFileName, outputFileName;
 
 	public AnalyticsCounter(String inputFileName, String outputFileName) {
-		this.inputFileName=inputFileName;
-		this.outputFileName=outputFileName;
+		this.inputFileName = inputFileName;
+		this.outputFileName = outputFileName;
 	}
-	
+
 	/**
 	 * 
 	 * @param inputFileName
@@ -28,36 +27,34 @@ public class AnalyticsCounter {
 		list = iSymptomReader.getSymptoms();
 		return list;
 	}
-	
-	
+
 	public Map<String, Integer> listToMap(List<String> list) {
-		
+
 		Map<String, Integer> map = new HashMap<String, Integer>();
-		for(String symptom : list) {
-			
-			if(!map.containsKey(symptom)) {
-				map.put(symptom,1);
+		for (String symptom : list) {
+
+			if (!map.containsKey(symptom)) {
+				map.put(symptom, 1);
+			} else {
+				map.put(symptom, map.get(symptom) + 1);
 			}
-			else {
-				map.put(symptom,map.get(symptom)+1);
-			}	
 		}
 
 		return map;
-		
+
 	}
-	
-	
-	public void writeSymptoms(String outputFileName, Map<String,Integer> map) throws IOException {
-	
-		this.outputFileName=outputFileName;
-			FileWriter writer = new FileWriter(outputFileName);
-			
-		for(Map.Entry<String,Integer> m:map.entrySet()){ 
-			
-			System.out.println(m.getKey()+"="+m.getValue()+";");
-			writer.write(m.getKey()+"="+m.getValue()+";"+"\n");;
-		  }  
-		writer.close();	
+
+	public void writeSymptoms(String outputFileName, Map<String, Integer> map) throws IOException {
+
+		this.outputFileName = outputFileName;
+		FileWriter writer = new FileWriter(outputFileName);
+
+		for (Map.Entry<String, Integer> m : map.entrySet()) {
+
+			System.out.println(m.getKey() + "=" + m.getValue() + ";");
+			writer.write(m.getKey() + "=" + m.getValue() + ";" + "\n");
+			;
+		}
+		writer.close();
 	}
 }
