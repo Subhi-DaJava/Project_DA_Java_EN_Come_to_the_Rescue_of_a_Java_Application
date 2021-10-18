9972605b97488f0b3535a9c306e04eaff5344aa9
Subhi-DaJava <subhyfr809@gmail.com>
2021-10-18 20:26:54 +0200

    petit changement

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomsCounter.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /**
- *Application lit les symptômes d'un fichier, les compte(le nombre d'occurrences) puis les affiche par ligne dasn un nouveau fichier de sortie ;  
+ * Application lit les symptômes d'un fichier, les compte(le nombre
+ * d'occurrences) puis les affiche par ligne dasn un nouveau fichier de sortie ;
+ * 
  * @author Subi
  *
  */
@@ -15,24 +17,22 @@ public class SymptomsCounter {
 
 	public static void main(String[] args) throws IOException {
 
-		
-			//Emplacement du fichier de sysmptômes
-			String inputFileName = "Project02Eclipse/symptoms.txt";
-			//Destination du fichier de sortie
-			String outputFileName = "result.out";
-			
-			List<String> symptomsList = new ArrayList<>();
-			Map<String, Integer> symptoms = new TreeMap<String, Integer>();
-			
-			//Une instance de la classe AnalyticsCounter
-			AnalyticsCounter analyticsCounter = new AnalyticsCounter(inputFileName, outputFileName);
-			//Liste reçoie les sysmptômes 
-			symptomsList = analyticsCounter.inputFile(inputFileName);
-			//Map(TreeMap) reçoie 
-			symptoms = analyticsCounter.getSymptoms(symptomsList);
-		
-			analyticsCounter.writeResult(outputFileName, symptoms);
-			
-	}
+		// Emplacement du fichier de sysmptômes
+		String inputFileName = "Project02Eclipse/symptoms.txt";
+		// Destination du fichier de sortie
+		String outputFileName = "result.out";
 
+		List<String> symptomsList = new ArrayList<>();
+		Map<String, Integer> symptoms = new TreeMap<String, Integer>();
+
+		// Une instance de la classe AnalyticsCounter
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter(inputFileName, outputFileName);
+		// Liste reçoie les sysmptômes
+		symptomsList = analyticsCounter.inputFile(inputFileName);
+		// Map(TreeMap) reçoie
+		symptoms = analyticsCounter.getSymptoms(symptomsList);
+
+		analyticsCounter.writeResult(outputFileName, symptoms);
+
+	}
 }
